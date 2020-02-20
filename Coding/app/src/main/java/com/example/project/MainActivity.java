cbe35db5a27627b96205c3203189f81b15e0c70e
Zikry <zickzikry777@gmail.com>
2020-02-20 22:19:18 +0800

    Part 3 dh siap (Login Function)

--- Coding/app/src/main/java/com/example/project/MainActivity.java
+++ Coding/app/src/main/java/com/example/project/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     private Button Login;
     private int counter = 5;
     private TextView userRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
 
 
     @Override
@@ -31,6 +43,16 @@ public class MainActivity extends AppCompatActivity {
         userRegistration = (TextView)findViewById(R.id.tvRegister);
 
         Info.setText("No of attempts remaining: 5");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user != null){
+            finish();
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        }
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,19 +71,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void validate(String userName, String userPassword){
-        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);
-        }
-        else{
-            counter--;
 
-            Info.setText("No of attempts remaining:" + String.valueOf(counter));
+        progressDialog.setMessage("Hy sayang");
+        progressDialog.show();
 
-            if(counter == 0){
-                Login.setEnabled(false);
+        firebaseAuth.signInWithEmailAndPassword(userName, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Login Succesfull", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                }
+
+                else {
+
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    counter--;
+                    Info.setText("No of attempts remaining: " + counter);
+                    progressDialog.dismiss();
+                    if(counter == 0){
+                        Login.setEnabled(false);
+                    }
+                }
             }
-        }
+        });
 
     }
+
 }
